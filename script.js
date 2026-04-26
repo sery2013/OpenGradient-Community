f14eb81a95a4dc1540a96c6c179f330adb9db53c
SERY2013.ETH <101403669+sery2013@users.noreply.github.com>
2026-04-27 00:53:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -304,8 +304,8 @@ try {
     // Рисуем логотип если загрузился
     if (logoImg.complete && logoImg.naturalWidth !== 0) {
         const logoSize = 55;  // Размер 45x45 пикселей
-        const logoX = (W / 2) - 330;  // СДВИНУЛ ЛЕВЕЕ (было -180, стало -220)
-        const logoY = H - 175;  // ПОДНЯЛ ВЫШЕ (было -175, стало -180)
+        const logoX = (W / 2) - 350;  // СДВИНУЛ ЛЕВЕЕ (было -180, стало -220)
+        const logoY = H - 180;  // ПОДНЯЛ ВЫШЕ (было -175, стало -180)
         
         ctx.drawImage(logoImg, logoX, logoY, logoSize, logoSize);
         console.log('🎨 Логотип нарисован на позиции:', logoX, logoY);
